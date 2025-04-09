219144cd52d930ede705a94ea4527376375f6c69
Bo Ren <boren@microsoft.com>
2025-04-09 17:21:46 +0000

    add draft dpo for phi4

---
+++ trl/scripts/dpo_bias.py
@@ -0,0 +1,80 @@
+# Copyright 2025 The HuggingFace Team. All rights reserved.
+#
+"""DPO training script."""
+
+import argparse
+from dataclasses import dataclass, field
+from typing import Optional
+from transformers import AutoModelForCausalLM, AutoProcessor
+from trl import DPOConfig, DPOTrainer, TrlParser
+from trl.scripts.audio_dataset import create_dataset
+
+
+@dataclass
+class DPOScriptArguments:
+    """Script arguments for the GRPO training script."""
+
+    dataset_name: str = field(metadata={"help": "Dataset name."})
+    job_name: Optional[str] = field(
+        default=None,
+        metadata={"help": "Name of the script."},
+    )
+    dataset_config: Optional[str] = field(
+        default=None,
+        metadata={"help": "Dataset config"},
+    )
+
+
+def init_model(model_id=None):
+    """Initialize the model and processor."""
+    model_id = model_id or "microsoft/Phi-4-multimodal-instruct"
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        trust_remote_code=True,
+        torch_dtype="auto",
+        _attn_implementation="flash_attention_2",
+    )
+    model.set_lora_adapter("speech")
+    processor = AutoProcessor.from_pretrained(
+        model_id,
+        trust_remote_code=True,
+    )
+    return model, processor
+
+
+def main(script_args, training_args):
+    model, processor = init_model()
+    dataset = create_dataset(
+        dataset_name=script_args.dataset_name,
+        **script_args.dataset_config,
+    )
+
+    trainer = DPOTrainer(
+        model,
+        None,
+        args=training_args,
+        train_dataset=dataset,
+        eval_dataset=None,
+        processing_class=processor,
+        # peft_config=peft_config,
+    )
+    print("Training...")
+    trainer.train()
+    print("Training complete.")
+
+
+def make_parser(subparsers: argparse._SubParsersAction = None):
+    dataclass_types = (DPOScriptArguments, DPOConfig)
+    if subparsers is not None:
+        parser = subparsers.add_parser(
+            "dpo", help="Run the DPO training script", dataclass_types=dataclass_types
+        )
+    else:
+        parser = TrlParser(dataclass_types)
+    return parser
+
+
+if __name__ == "__main__":
+    parser = make_parser()
+    script_args, training_args = parser.parse_args_and_config()
+    main(script_args, training_args)
