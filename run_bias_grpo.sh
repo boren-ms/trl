1e3abacb38b4971495aaaee180be2c1af990af01
Bo Ren <boren@microsoft.com>
2025-04-09 04:57:53 +0000

    update eval

--- run_bias_grpo.sh
+++ run_bias_grpo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-set -x 
+set -x
 
-python trl/scripts/grpo_bias.py --config exp_conf/ls_biasing.yaml --output_dir ./output_bias
+python trl/scripts/grpo_bias.py --config exp_conf/ls_train_biasing.yaml --output_dir ./output_bias
