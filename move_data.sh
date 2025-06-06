3aab231f3a090f18652f536b0e6bebd1515b943a
Bo Ren <boren@microsoft.com>
2025-06-05 19:34:34 -0700

    update data move script

--- move_data.sh
+++ move_data.sh
@@ -4,5 +4,9 @@ set -x
 region="wus2"
 remote_dir="az://orng${region}cresco/data/boren/data"
 local_dir="/root/data"
+# download data from Azure Blob Storage to local directory
+# bbb sync --delete --concurrency 32 $remote_dir $local_dir
 
-bbb sync --delete --concurrency 32 $remote_dir $local_dir
+# upload data to remote server
+local_dir=/home/boren/data/LibriSpeech
+bbb sync --delete --concurrency 32 $local_dir $remote_dir
