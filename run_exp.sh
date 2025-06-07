ab8e4337616b73b58e2345b03021a92028812357
Bo Ren <boren@microsoft.com>
2025-06-07 13:15:19 -0700

    update run_exp

--- run_exp.sh
+++ run_exp.sh
@@ -5,8 +5,12 @@ set -x
 # login to wandb
 # wandb login --relogin --host=https://msaip.wandb.io
 
+host=$(hostname)
+task=${host%-*}
+output_dir=/root/results/${task}/${conf}
+
 conf=sft_grpo_debug
 
 bash run_mpi.sh trl/scripts/grpo_bias.py \
-     --output_dir ./output/${conf} \
+     --output_dir ${output_dir} \
      --config orng_conf/${conf}.yaml
