518c30d64e7f4d042c35b66f04cffa3e60d90e8b
Bo Ren <boren@microsoft.com>
2025-06-07 13:48:19 -0700

    update script

--- run_exp.sh
+++ run_exp.sh
@@ -3,14 +3,16 @@
 set -x
 
 # login to wandb
-export WANDB_API_KEY=cefd6cab73652e59732d3cfc4f776050f88f08f8
-export WANDB_ORGANIZATION=https://msaip.wandb.io
+# export WANDB_API_KEY=cefd6cab73652e59732d3cfc4f776050f88f08f8
+# export WANDB_ORGANIZATION=https://msaip.wandb.io
+export WANDB_DISABLED="true" 
 # wandb login --relogin --host=https://msaip.wandb.io
 
 conf=sft_grpo_debug
-output_dir=${RCALL_LOGDIR}/${conf}
+OUTPUT_DIR=${RCALL_LOGDIR}/${conf}
+export WANDB_DIR=${OUTPUT_DIR}/wandb
 
 
 bash run_mpi.sh trl/scripts/grpo_bias.py \
-     --output_dir ${output_dir} \
+     --output_dir ${OUTPUT_DIR} \
      --config orng_conf/${conf}.yaml
