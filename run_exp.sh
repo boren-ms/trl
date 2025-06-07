1a0fc2365ed86dac090a17a1976d4e222cd6e7b7
Bo Ren <boren@microsoft.com>
2025-06-07 15:45:06 -0700

    refine exp run

--- run_exp.sh
+++ run_exp.sh
@@ -8,10 +8,17 @@ set -x
 export WANDB_DISABLED="true" 
 # wandb login --relogin --host=https://msaip.wandb.io
 
-conf=sft_grpo_debug
-OUTPUT_DIR=${RCALL_LOGDIR}/${conf}
-# export WANDB_DIR=${OUTPUT_DIR}
 
+config_file=orng_conf/grpo_bias_debug.yaml
+name=grpo_bias_debug
+
+# config_file=orng_conf/grpo_bias_librispeech_v0.yaml
+# name=grpo_bias_v0_n12_b20k
+
+
+
+
+OUTPUT_DIR=${RCALL_LOGDIR}/${name}
 echo "
 export OUTPUT_DIR=${OUTPUT_DIR}
 " >>~/.bashrc
@@ -21,4 +28,4 @@ bash prepare_orng.sh
 
 bash run_mpi.sh trl/scripts/grpo_bias.py \
      --output_dir ${OUTPUT_DIR} \
-     --config orng_conf/${conf}.yaml
+     --config ${config_file}
