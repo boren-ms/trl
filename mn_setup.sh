6cd43f93f1091bd08251d0f8ffcea9623b6f8e26
Bo Ren <boren@microsoft.com>
2025-06-07 12:48:31 -0700

    refine script

--- mn_setup.sh
+++ mn_setup.sh
@@ -1,10 +1,14 @@
 #%!/bin/bash
 
-target=$(hostname | sed 's/.$//')
+host=$(hostname)
+target=${host::-1}
+echo "Hostname: ${host}"
+echo "Node:${RCALL_INSTANCE_COUNT}, GPU:${RCALL_NUM_GPU}"
 
-N=7 #last node
+export N=${RCALL_INSTANCE_COUNT}
+
 code_dir=/root/code/trl/
-for i in $(seq 0 $N); do
+for i in $(seq 0 $((N-1))); do
     echo "Remotely run ${code_dir}/install.sh on ${target}${i}"
     ssh ${target}${i} "export PATH=/root/.pyenv/versions/3.11.8/bin/:\$PATH; bash ${code_dir}/install.sh > ${code_dir}/install.log 2>&1 & "
 done
--- move_data.sh
+++ move_data.sh
@@ -2,13 +2,18 @@
 target=$(hostname | sed 's/.$//')
 
 data_dir="/root/data"
-N=7 #last node
 
+host=$(hostname)
+target=${host::-1}
+echo "Hostname: ${host}"
+echo "Node:${RCALL_INSTANCE_COUNT}, GPU:${RCALL_NUM_GPU}"
+
+export N=${RCALL_INSTANCE_COUNT}
 region="wus2"
 remote_dir="az://orng${region}cresco/data/boren/data"
 bbb sync --delete --concurrency 32 $remote_dir $data_dir
 echo "Data moved successfully to $data_dir"
-for i in $(seq 1 $N); do
+for i in $(seq 1 $((N-1))); do
     echo "Move data to ${target}${i}"
     nohup rsync -avz $data_dir ${target}${i}:$(dirname $data_dir) > rsync${i}.log 2>&1 &
 done
