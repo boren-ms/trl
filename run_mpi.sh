4e5660739fa786bbd7e47f2daefb050a42ca6946
Bo Ren <boren@microsoft.com>
2025-06-07 15:34:20 -0700

    fix run_mpi

--- run_mpi.sh
+++ run_mpi.sh
@@ -9,17 +9,20 @@ echo "Node:${RCALL_INSTANCE_COUNT}, GPU:${RCALL_NUM_GPU}"
 export NUM_NODE=${RCALL_INSTANCE_COUNT}
 export NUM_GPU=${RCALL_NUM_GPU}
 export MASTER_ADDR=${RCALL_HOSTNAME}
-export MASTER_PORT=12345
+export MASTER_PORT=1235
 
 # generate hostfile
 for i in $(seq 0 $((NUM_NODE-1))); do echo "${JOB_NAME}-$i"; done > "hostfile"
 
 # mpirun launch
-mpirun --f "hostfile" -np ${NUM_NODE} \
+mpirun -f "hostfile" -np ${NUM_NODE} \
     bash run_dist.sh $@
 
-# Copy the necessary scripts to each node
-# for i in $(seq 1 $((NUM_NODE-1))); do
-#     scp /root/code/trl/run_dist.sh ${JOB_NAME}-$i:/root/code/trl/run_dist.sh
-#     scp /root/code/trl/trl/scripts/grpo_bias.py ${JOB_NAME}-$i:/root/code/trl/trl/scripts/grpo_bias.py
+# export PMI_SIZE=$NUM_NODE
+# for i in $(seq 0 $((NUM_NODE-1))); do
+#     export PMI_RANK=${i}
+#     ssh ${JOB_NAME}-${i} bash run_dist.sh $@
 # done
+
+
+# for i in $(seq 0 $((NUM_NODE-1))); do     ssh "${JOB_NAME}-${i}" sudo hostname;pgrep -fa bias; done
