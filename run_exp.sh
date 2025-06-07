5fa5ef8ee99a86b5c7bcd79f5fa77daa61a960c9
Bo Ren <boren@microsoft.com>
2025-06-07 12:39:43 -0700

    update script

--- run_exp.sh
+++ run_exp.sh
@@ -5,7 +5,8 @@ set -x
 # login to wandb
 # wandb login --relogin --host=https://msaip.wandb.io
 
+conf=sft_grpo_debug
 
 bash run_mpi.sh trl/scripts/grpo_bias.py \
-     --output_dir ./output \
-     --config orng_conf/sft_grpo_ls_train_n12_err_lr_v0.yaml
+     --output_dir ./output/${conf} \
+     --config orng_conf/${conf}.yaml
