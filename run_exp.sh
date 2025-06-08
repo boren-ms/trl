420c3d7a32a45a1a5430c845b834853915fe0933
Bo Ren <boren@microsoft.com>
2025-06-07 23:50:39 -0700

    update script

--- run_exp.sh
+++ run_exp.sh
@@ -2,23 +2,29 @@
 
 set -x
 
-# login to wandb
-# export WANDB_API_KEY=cefd6cab73652e59732d3cfc4f776050f88f08f8
-# export WANDB_ORGANIZATION=https://msaip.wandb.io
+JOB_NAME=${RCALL_JOB_NAME}
+export NUM_NODE=${RCALL_INSTANCE_COUNT}
 export WANDB_DISABLED="true" 
-# wandb login --relogin --host=https://msaip.wandb.io
+code_dir=/root/code/trl
 
 
-config_file=orng_conf/grpo_bias_debug.yaml
+
+config_file=${code_dir}/orng_conf/grpo_bias_debug.yaml
 name=grpo_bias_debug
 
-# config_file=orng_conf/grpo_bias_librispeech_v0.yaml
+
+# config_file=${code_dir}/orng_conf/grpo_bias_librispeech_v0.yaml
 # name=grpo_bias_v0_n12_b20k
 
+echo "sync ${config_file}"
+for i in $(seq 1 $((NUM_NODE-1))); do
+    rsync -avz ${config_file} ${JOB_NAME}-${i}:${config_file}
+done
 
-export USE_ACCELERATE=true
 
+export USE_ACCELERATE=false
 OUTPUT_DIR=${RCALL_LOGDIR}/${name}
+
 echo "
 export OUTPUT_DIR=${OUTPUT_DIR}
 " >>~/.bashrc
@@ -27,5 +33,4 @@ export OUTPUT_DIR=${OUTPUT_DIR}
 bash prepare_orng.sh
 
 bash run_mpi.sh trl/scripts/grpo_bias.py \
-     --output_dir ${OUTPUT_DIR} \
-     --config ${config_file}
+     --output_dir ${OUTPUT_DIR}  --config ${config_file}
