40facb8a4b1c348f814eec2dac95a49c2e6d1ca1
Bo Ren <boren@microsoft.com>
2025-06-07 20:29:31 -0700

    use torchrun

--- run_dist.sh
+++ run_dist.sh
@@ -5,24 +5,24 @@ set -euo pipefail
 wandb offline
 # printenv
 
-# cmd="
-# torchrun \
-#     --nnodes=${PMI_SIZE} \
-#     --node_rank ${PMI_RANK} \
-#     --nproc-per-node=${NUM_GPU} \
-#     --master_addr ${MASTER_ADDR} \
-#     --master_port ${MASTER_PORT} \
-#     $@
-# "
 cmd="
-accelerate launch \
-    --num_processes $(($PMI_SIZE*$NUM_GPU)) \
-    --num_machines ${PMI_SIZE} \
-    --machine_rank ${PMI_RANK} \
-    --main_process_ip ${MASTER_ADDR} \
-    --main_process_port ${MASTER_PORT} \
+torchrun \
+    --nnodes=${PMI_SIZE} \
+    --node_rank ${PMI_RANK} \
+    --nproc-per-node=${NUM_GPU} \
+    --master_addr ${MASTER_ADDR} \
+    --master_port ${MASTER_PORT} \
     $@
 "
+# cmd="
+# accelerate launch \
+#     --num_processes $(($PMI_SIZE*$NUM_GPU)) \
+#     --num_machines ${PMI_SIZE} \
+#     --machine_rank ${PMI_RANK} \
+#     --main_process_ip ${MASTER_ADDR} \
+#     --main_process_port ${MASTER_PORT} \
+#     $@
+# "
 log_file=rank_${PMI_RANK}.log
 echo "Running $cmd" > $log_file
 # printenv >> $log_file
