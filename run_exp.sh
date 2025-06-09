9ed0232330265e5ff7474b58363ba5721b521834
Bo Ren <boren@microsoft.com>
2025-06-08 18:34:34 -0700

    update exp script

--- run_exp.sh
+++ run_exp.sh
@@ -11,23 +11,21 @@ export DATA_DIR=/root/data
 export USE_ACCELERATE=false
 export WANDB_DISABLED=true
 
+# export EXP_CONFIG=${CODE_DIR}/orng_conf/grpo_bias_debug.yaml
+# export EXP_NAME=grpo_bias_debug
 
-export EXP_CONFIG=${CODE_DIR}/orng_conf/grpo_bias_debug.yaml
-export EXP_NAME=grpo_bias_debug
-
-export OUTPUT_DIR=${RCALL_LOGDIR}/${EXP_NAME}
-
-mkdir -p ${OUTPUT_DIR}
+export EXP_CONFIG=${CODE_DIR}/orng_conf/grpo_bias_librispeech_b100k_v1.yaml
+export EXP_NAME=grpo_bias_v0_n12_b20k
 
 
-# EXP_CONFIG=${CODE_DIR}/orng_conf/grpo_bias_librispeech_v0.yaml
-# NAME=grpo_bias_v0_n12_b20k
 
 echo "sync ${EXP_CONFIG}"
 for i in $(seq 1 $((NUM_NODE-1))); do
     rsync -avz ${EXP_CONFIG} ${JOB_NAME}-${i}:${EXP_CONFIG}
 done
 
+export OUTPUT_DIR=${RCALL_LOGDIR}/${EXP_NAME}
+mkdir -p ${OUTPUT_DIR}
 
 # bash prepare_orng.sh --force
 bash prepare_orng.sh
