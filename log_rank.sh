d8ad18c1d86ac3c2c5ac22321a3000be372d6e80
Bo Ren <boren@microsoft.com>
2025-06-12 10:14:42 -0700

    revert to log rank logs

--- log_rank.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# dist_train.sh
-set -euo pipefail
-
-RANK=${MPI_RANK:-0}
-RANK_LOG_FILE=${RCALL_LOGDIR}/*/rank_${RANK}.log
-echo "[${RANK} ] Tail ${RANK_LOG_FILE}"
-tail -n 30 $RANK_LOG_FILE
