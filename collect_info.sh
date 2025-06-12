098e21401055c51738c90ed836cdbd42ce027575
Bo Ren <boren@microsoft.com>
2025-06-12 09:42:05 -0700

    update collection

--- collect_info.sh
+++ collect_info.sh
@@ -2,11 +2,11 @@
 
 
 echo "Collecting process information..."
-bash ./mpi_bash.sh pgrep -fa grpo_bias | tee proc.txt
+bash ./mpi_bash.sh pgrep -fa grpo_bias | tee procs.txt
 
 echo "Collecting GPUs information..."
 bash ./mpi_bash.sh nvidia-smi|grep Default | tee gpus.txt
 
 echo "Collecting logs from all ranks..."
 echo "Log directory: ${RCALL_LOGDIR}"
-bash ./mpi_bash.sh "tail -n 100 ${RCALL_LOGDIR}/*/rank_*.log" | tee proc.txt
+bash ./mpi_bash.sh "tail -n 30 ${RCALL_LOGDIR}/*/rank_*.log" | tee ranks.txt
