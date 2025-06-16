5dd7bf0811e8ea7bb0cbd8c211f5ac5c00b69f89
Bo Ren <boren@microsoft.com>
2025-06-16 16:58:24 -0700

    update collect  info

--- collect_info.sh
+++ collect_info.sh
@@ -9,4 +9,11 @@ bash ./mpi_bash.sh nvidia-smi|grep Default | tee gpus.txt
 
 echo "Collecting logs from all ranks..."
 echo "Log directory: ${RCALL_LOGDIR}"
-bash ./mpi_bash.sh "tail -n 30 ${RCALL_LOGDIR}/*/rank_*.log" | tee ranks.txt
+
+echo 'Writing log' > ranks.txt
+for i in $(seq 0 $((RCALL_INSTANCE_COUNT-1))); do
+    
+    echo "Logging to ${RCALL_JOB_NAME}-${i}" >> ranks.txt
+    ssh ${RCALL_JOB_NAME}-${i} "tail -n 30  ${RCALL_LOGDIR}/*/rank_${i}.log" >> ranks.txt
+    echo "" >> ranks.txt
+done
