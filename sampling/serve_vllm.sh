17a89edd6ca9bb910e688b7115d95d430bc0eb48
Bo Ren <boren@microsoft.com>
2025-06-28 23:28:39 +0000

    add lora merge

--- sampling/serve_vllm.sh
+++ sampling/serve_vllm.sh
@@ -10,6 +10,7 @@ set -x
 # model_path=/home/boren/data/hf_models/Phi-4-7b-ASR-merged
 # model_path=/home/boren/data/hf_models/Phi-4-multimodal-instruct
 # model_path=/home/boren/data/hf_models/phi4_mm_bias_merged
+# use sampling/merge_hf_lora.py to merge lora weights into the model.
 model_path=/home/boren/data/hf_models/phi-libri_ft_m1000_p8_new-QpHq/5000_hf_merged
 vllm serve ${model_path} --host 127.0.0.1 --port 26500 --tensor-parallel-size 1 --trust-remote-code --load-format auto --max-model-len 8192 --limit-mm-per-prompt audio=10
 
