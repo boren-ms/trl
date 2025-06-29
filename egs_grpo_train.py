163d99af9944345ea2f43db8be0633b7be62aad7
Bo Ren <boren@microsoft.com>
2025-06-28 19:35:04 -0700

    add egs

--- egs_grpo_train.py
+++ egs_grpo_train.py
@@ -1,29 +1,30 @@
-# %%
+#%%
+
+# trl vllm-serve --model Qwen/Qwen2.5-0.5B --tensor-parallel-size 2 --data-parallel-size 
+# trl vllm-serve --model /home/boren/data//ckp/hf_models/Qwen2.5-0.5B-Instruct
+
+# client/trainer
+# cd ~/code # get out of the trl dir
+# cp trl/egs_grpo_vllm.py ~/code
+# CUDA_VISIBLE_DEVICES=4 python  egs_grpo_vllm.py
+#%%
 from datasets import load_dataset
 from trl import GRPOTrainer, GRPOConfig
 
 # dataset = load_dataset("trl-lib/tldr", split="train")
 
-data_path = "/home/boren/data/gsm8k"
-dataset = load_dataset(
-    data_path,
-    data_files={"train": "train.parquet", "test": "test.parquet"},
-    split="train",
-)
+data_path="/home/boren/data/gsm8k"
+dataset = load_dataset(data_path, data_files={"train": "train.parquet", "test": "test.parquet"}, split="train")
 
-model_path = "/home/boren/data//ckp/Qwen/models--Qwen--Qwen2.5-0.5B-Instruct"
-
-
+model_path="/home/boren/data//ckp/hf_models/Qwen2.5-0.5B-Instruct"
 # model_path="Qwen/Qwen2.5-7B",
 # Dummy reward function: count the number of unique characters in the completions
 def reward_num_unique_chars(completions, **kwargs):
-    """Reward function that counts the number of unique characters in the completions."""
     return [len(set(c)) for c in completions]
-
 
 training_args = GRPOConfig(
     output_dir="my_test",
-    use_vllm=True,
+    use_vllm=True, #use vllm for generation.
     bf16=True,
     gradient_checkpointing=True,
 )
@@ -36,4 +37,4 @@ trainer = GRPOTrainer(
 )
 
 trainer.train()
-# %%
+#%%
