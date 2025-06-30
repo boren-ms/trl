fb82de17a766e77de255312ad53b2af6c26003b3
Bo Ren <boren@microsoft.com>
2025-06-30 14:11:59 -0700

    update quick setup

--- quick_prepare.sh
+++ quick_prepare.sh
@@ -6,11 +6,22 @@ REMOTE_DIR="az://orngwus2cresco/data/boren/data"
 LOCAL_DIR="/home/boren/data/"
 
 
-rel_paths=(
-    "ckp/Qwen/models--Qwen--Qwen2.5-0.5B-Instruct"   
-    "gsm8k"
+rel_dirs=(
+    "gsm8k",
+    "ckp/hf_models/Qwen2.5-0.5B-Instruct"
+    "ckp/hf_models/phi4_mm_bias_merged",
+    "ckp/hf_models/phi4_mm_bias",
+    "LibriSpeech/train-clean-360/115/122944/115-122944"
 )
 
-for rel_path in "${rel_paths[@]}"; do
-    bbb sync "$REMOTE_DIR/$rel_path" "$LOCAL_DIR/$rel_path"
+for rel_dir in "${rel_dirs[@]}"; do
+    bbb sync "$REMOTE_DIR/$rel_dir" "$LOCAL_DIR/$rel_dir"
+done
+
+rel_files=(
+   LibriSpeech/ls_30k_shuf.tsv
+   LibriSpeech/debug.tsv
+)
+for rel_file in "${rel_files[@]}"; do
+    bbb cp "$REMOTE_DIR/$rel_file" "$LOCAL_DIR/$rel_file"
 done
