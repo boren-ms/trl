4416974eef0ad3a81866f4292f6a9d22f9772c74
Bo Ren <boren@microsoft.com>
2025-06-30 13:53:46 -0700

    update quick setups

--- quick_install.sh
+++ quick_install.sh
@@ -1,3 +1,3 @@
 pip uninstall -y torch torchvision torchaudio transformers flash-attn vllm trl
-pip install  torch==2.6.0 torchvision torchaudio transformers vllm trl 
+pip install  torch==2.6.0 torchvision torchaudio transformers==4.51.3 vllm trl 
 pip install  torch==2.6.0 flash-attn 
