1afffc80314bfce215ceb8d9f8fd75c10f0211b8
Bo Ren <boren@microsoft.com>
2025-07-01 14:42:50 -0700

    fix serve vllm

--- sampling/trl_serve_vllm.sh
+++ sampling/trl_serve_vllm.sh
@@ -9,10 +9,11 @@ set -x
 
 # model_path=${HOME}/data/ckp/hf_models/Phi-4-7b-ASR-merged
 # model_path=${HOME}/data/ckp/hf_models/Phi-4-multimodal-instruct
-# model_path=${HOME}/data/ckp/hf_models/phi4_mm_bias_merged
+model_path=${HOME}/data/ckp/hf_models/phi4_mm_bias_merged
 # use sampling/merge_hf_lora.py to merge lora weights into the model.
-model_path=${HOME}/data/ckp/hf_models/phi-libri_ft_m1000_p8_new-QpHq/5000_hf_merged
-vllm serve ${model_path} --host 127.0.0.1 --port 26500 --tensor-parallel-size 1 --trust-remote-code --load-format auto --max-model-len 8192 --limit-mm-per-prompt audio=10
+# model_path=${HOME}/data/ckp/hf_models/phi-libri_ft_m1000_p8_new-QpHq/5000_hf_merged
+trl vllm-serve --model ${model_path} --host 127.0.0.1 --port 8000 --tensor-parallel-size 1 --trust-remote-code --max-model-len 8192 
+# --load-format auto --limit-mm-per-prompt audio=10
 
 # with lora
 # model_path=${HOME}/data/ckp/hf_models/Phi-4-multimodal-instruct
