c747e0b5c110ce7f1bc79276f60661e5714b9f7c
Bo Ren <boren@microsoft.com>
2025-07-02 15:45:25 -0700

    update scripts

---
+++ quick_run_exp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+set -x 
+
+model_path=${HOME}/data/ckp/hf_models/phi4_mm_bias_merged/
+
+echo "Running grpo_bias with ${model_path} on 4 GPUs"
+# CUDA_VISIBLE_DEVICES=4,5,6,7 
+export WANDB_MODE=offline
+CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch trl/scripts/grpo_bias.py --config orng_conf/grpo_bias_debug.yaml
