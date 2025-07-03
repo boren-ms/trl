360fa73263e82545a22096c372c5e5a83cd039d3
Bo Ren <boren@microsoft.com>
2025-07-03 14:33:58 -0700

    update quick install

--- quick_install.sh
+++ quick_install.sh
@@ -1,6 +1,6 @@
-uv pip uninstall -y torch torchvision torchaudio transformers flash-attn vllm trl
-uv pip install  torch==2.6.0 torchvision torchaudio transformers==4.51.3 vllm trl peft tensorboardX blobfile soundfile flash-attn==2.8.0.post2
-uv pip install  torch==2.6.0 flash-attn
-# uv pip install  torch==2.6.0 flashinfer-python==0.2.6
-uv pip uninstall -y trl
-uv pip install -e . --no-deps
+pip uninstall -y torch torchvision torchaudio transformers flash-attn vllm trl
+uv pip install --system  torch==2.6.0 torchvision torchaudio transformers==4.51.3 vllm trl peft tensorboardX blobfile soundfile
+pip install   torch==2.6.0 flash-attn
+# uv pip install --system  torch==2.6.0 flashinfer-python==0.2.6
+pip uninstall -y trl
+pip install --system  -e . --no-deps
