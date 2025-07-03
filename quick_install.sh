c2a909c8f8d362140bb2a76fe03913af1be7a0b2
Bo Ren <boren@microsoft.com>
2025-07-03 14:48:49 -0700

    fix quick install

--- quick_install.sh
+++ quick_install.sh
@@ -3,4 +3,4 @@ uv pip install --system  torch==2.6.0 torchvision torchaudio transformers==4.51.
 pip install   torch==2.6.0 flash-attn
 # uv pip install --system  torch==2.6.0 flashinfer-python==0.2.6
 pip uninstall -y trl
-pip install --system  -e . --no-deps
+uv pip install --system  -e . --no-deps
