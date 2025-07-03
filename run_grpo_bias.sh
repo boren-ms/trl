6257655b26c70b8abd02076c05f06a884fcecfd3
Bo Ren <boren@microsoft.com>
2025-07-03 15:01:56 -0700

    update logg

--- run_grpo_bias.sh
+++ run_grpo_bias.sh
@@ -23,6 +23,8 @@ if [[ -z "${VLLM_SERVER_HOST}" ]]; then
     echo "Error: VLLM_SERVER_HOST environment variable is not set."
     echo "Please run the following command to start the vllm server in a separate node for vllm generation:"
     echo "bash quick_vllm_serve.sh"
+    echo "You can set it by running in terminal:"
+    echo "export VLLM_SERVER_HOST="
     exit 1
 fi
 
