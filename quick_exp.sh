39ccedbe7ec04fb18aee90718478b354c7eb1b75
Bo Ren <boren@microsoft.com>
2025-07-02 18:46:35 -0700

    update quick exp

--- quick_exp.sh
+++ quick_exp.sh
@@ -5,7 +5,7 @@ model_path=${HOME}/data/ckp/hf_models/phi4_mm_bias_merged/
 
 echo "Running grpo_bias with ${model_path} on 4 GPUs"
 # CUDA_VISIBLE_DEVICES=4,5,6,7 
-export WANDB_MODE=offline
-wandb offline
+# wandb offline
+# wandb login --relogin --host=https://msaip.wandb.io
 # CUDA_VISIBLE_DEVICES=4 accelerate launch trl/scripts/grpo_bias.py --config orng_conf/grpo_bias_debug.yaml
 CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch trl/scripts/grpo_bias.py --config orng_conf/grpo_bias_debug.yaml
--- quick_exp_multi_nodes.sh
+++ quick_exp_multi_nodes.sh
@@ -5,8 +5,9 @@ model_path=${HOME}/data/ckp/hf_models/phi4_mm_bias_merged/
 
 echo "Running grpo_bias with ${model_path} on 4 GPUs"
 # CUDA_VISIBLE_DEVICES=4,5,6,7 
-export WANDB_MODE=offline
-wandb offline
+# wandb offline
+# wandb login --relogin --host=https://msaip.wandb.io
+
 # CUDA_VISIBLE_DEVICES=4 accelerate launch trl/scripts/grpo_bias.py --config orng_conf/grpo_bias_debug.yaml
 # use all GPUs available
 accelerate launch trl/scripts/grpo_bias.py --config orng_conf/grpo_bias_debug_multi_nodes.yaml
