73640ad340952c2b47ddb663e63e142d7dc0d6b9
Bo Ren <boren@microsoft.com>
2025-07-04 14:51:20 -0700

    update quick data

--- quick_data.sh
+++ quick_data.sh
@@ -12,15 +12,19 @@ export DATA_STORAGE=${region_storages[$CLUSTER_REGION]}
 
 REMOTE_DIR="az://${DATA_STORAGE}/data/boren/data"
 # REMOTE_DIR="az://orngwus2cresco/data/boren/data"
+# REMOTE_DIR="az://orngscuscresco/data/boren/data"
 LOCAL_DIR="${HOME}/data"
 
 
 rel_dirs=(
-    "gsm8k"
-    "ckp/hf_models/Qwen2.5-0.5B-Instruct"
+    # "gsm8k"
+    # "ckp/hf_models/Qwen2.5-0.5B-Instruct"
     "ckp/hf_models/phi-libri_ft_m1000_p8_new-QpHq/5000_hf_merged"
     "ckp/hf_models/phi4_mm_bias_merged"
-    "ckp/hf_models/phi4_mm_bias"
+    # "ckp/hf_models/phi4_mm_bias"
+    "librispeech_biasing/words"
+    "librispeech_biasing/ref"
+    "librispeech_biasing/hyp"
     "LibriSpeech/test-clean"
     "LibriSpeech/train-clean-360/115/122944"
 )
