31e15fed2513913f9519243d3484f31a21f01b60
Bo Ren <boren@microsoft.com>
2025-07-04 21:37:40 -0700

    add package

--- quick_install.sh
+++ quick_install.sh
@@ -1,5 +1,5 @@
 pip uninstall -y torch torchvision torchaudio transformers flash-attn vllm trl
-uv pip install --system  torch==2.6.0 torchvision torchaudio transformers==4.51.3 vllm trl peft tensorboardX blobfile soundfile
+uv pip install --system  torch==2.6.0 torchvision torchaudio transformers==4.51.3 vllm trl peft tensorboardX blobfile soundfile more-itertools
 pip install   torch==2.6.0 flash-attn
 # uv pip install --system  torch==2.6.0 flashinfer-python==0.2.6
 pip uninstall -y trl
