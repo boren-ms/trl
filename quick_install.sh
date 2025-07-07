80572a55473ff1f2ab422fb22a6bfa322e9be9ad
Bo Ren <boren@microsoft.com>
2025-07-07 16:59:53 -0700

    update whisper norm install

--- quick_install.sh
+++ quick_install.sh
@@ -1,5 +1,5 @@
 pip uninstall -y torch torchvision torchaudio transformers flash-attn vllm trl
-uv pip install --system  torch==2.6.0 torchvision torchaudio transformers==4.51.3 vllm trl peft tensorboardX blobfile soundfile more-itertools
+uv pip install --system  torch==2.6.0 torchvision torchaudio transformers==4.51.3 vllm trl peft tensorboardX blobfile soundfile more-itertools whisper_normalizer
 pip install   torch==2.6.0 flash-attn
 # uv pip install --system  torch==2.6.0 flashinfer-python==0.2.6
 pip uninstall -y trl
