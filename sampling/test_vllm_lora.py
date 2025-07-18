21a4350f36f66e0d115efac8988efddec9d6a08b
Bo Ren <boren@microsoft.com>
2025-07-18 10:33:11 -0700

    refine test code

--- sampling/test_vllm_lora.py
+++ sampling/test_vllm_lora.py
@@ -5,6 +5,12 @@ from vllm.lora.request import LoRARequest
 import vllm
 from pathlib import Path
 from vllm import LLM, SamplingParams
+import torch
+from transformers import GenerationConfig, AutoProcessor
+from vllm import LLM, SamplingParams
+from trl.data_utils import sf_read
+from datasets import load_dataset  # Added import
+
 
 # %%
 
@@ -15,7 +21,7 @@ model_path = Path("/root/data/ckp/hf_models/Phi-4-multimodal-instruct")
 # %%
 # HuggingFace model name
 
-lora_path = model_path / "speech-lora"
+speech_lora_path = model_path / "speech-lora"
 # Create the LLM object with LoRA adaptation
 llm = LLM(
     model=str(model_path),
@@ -23,22 +29,29 @@ llm = LLM(
     max_model_len=4096,
     max_num_seqs=2,
     enable_lora=True,
-    limit_mm_per_prompt={"audio": 1},
+    limit_mm_per_prompt={"audio": 2},
+    max_lora_rank=320,
 )
+#%%
+# prompt = "What is the capital of France?"
+# outputs = llm.generate([prompt], SamplingParams(temperature=1, max_tokens=640))
+# for output in outputs:
+#     print(output.outputs[0].text)
+# %%
 
 prompts = [
     "<|user|><|audio_1|>Transcribe the audio clip into text. <|end|><|assistant|>"
 ]
 audios = ["/root/data/LibriSpeech/train-clean-360/115/122944/115-122944-0038.flac"]
-# prompts = ["Hello, AI!", "Tell me a joke"]
-# Model paths and processor
-# Example prompt
-prompt = "What is the capital of France?"
-# Set sampling parameters
+inputs = [
+        {"prompt": prompt, "multi_modal_data": {"audio": [sf_read(audio_path)]}}
+        for prompt, audio_path in zip(prompts, audios)
+]
 sampling_params = SamplingParams(temperature=1, max_tokens=640)
-# Generate output
-outputs = llm.generate([prompt], sampling_params)
-# Print the generated text
-for output in outputs:
-    print(output.outputs[0].text)
+#%%
+lora_request = [LoRARequest("speech", 1, str(speech_lora_path))]
+outputs = llm.generate(inputs, sampling_params=sampling_params,  lora_request=lora_request)
+texts = [output.outputs[0].text for output in outputs]
+text = texts[0]
+print(text)
 # %%
