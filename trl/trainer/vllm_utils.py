e2f489dbc07d8c862b188dbd7043cdfa0aa1a0cc
Bo Ren <boren@microsoft.com>
2025-07-26 19:05:11 -0700

    load lora weight

--- trl/trainer/vllm_utils.py
+++ trl/trainer/vllm_utils.py
@@ -163,3 +163,5 @@ def update_vllm_lora(llm, model, alpha, r, target_modules=None):
         lora_tensors=lora_params,
     )
     llm.llm_engine.add_lora(lora_request)
+    llm_model = llm.llm_engine.model_executor.driver_worker.model_runner.model
+    llm_model.load_weights([(name, param) for name, param in lora_params.items()])
