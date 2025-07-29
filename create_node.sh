3f4deb6e305bbfa55a1c13c2cbbbff9355d051bd
Bo Ren <boren@microsoft.com>
2025-07-29 04:54:17 +0000

    udpate

--- create_node.sh
+++ create_node.sh
@@ -1,7 +1,7 @@
 OAIPKG_OVERRIDE_WHEEL=unsafe_skip \
-BRIX_QUOTA='team-moonfire-m365' twdev create-ray-devbox cluster="prod-westus2-19" \
+BRIX_QUOTA='team-moonfire-m365' twdev create-ray-devbox cluster=prod-southcentralus-hpe-4  \
 setup_twapi=True \
-num_pods=1 \
+num_pods=4 \
 num_gpu=8 \
-job_name=bus-grader \
-priority_class=team-high 
+job_name=h-n4-hpe4 \
+priority_class=team-high
