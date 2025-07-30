c0ff9f7f77eb2ac33a1b9154ccc2395d6064cb93
Bo Ren <boren@microsoft.com>
2025-07-30 19:36:46 +0000

    update docker temp

--- build_docker.sh
+++ build_docker.sh
@@ -1,8 +1,13 @@
 # az login
 # az acr login -n sramdevregistry -g devboxes
-set -x
-docker build --build-arg WANDB_API_KEY="${WANDB_API_KEY}" -t sramdevregistry.azurecr.io/boren_dev:trl -f Dockerfile.trl .
-docker push sramdevregistry.azurecr.io/boren_dev:trl
+# set -x
+# docker build --build-arg WANDB_API_KEY="${WANDB_API_KEY}" -t sramdevregistry.azurecr.io/boren_dev:trl -f Dockerfile.trl .
+# docker push sramdevregistry.azurecr.io/boren_dev:trl
+# set -x
+
+tag=boren_trl
+docker build --build-arg WANDB_API_KEY="${WANDB_API_KEY}" -t sramdevregistry.azurecr.io/boren_dev:${tag} -f Dockerfile.${tag} .
+docker push sramdevregistry.azurecr.io/boren_dev:${tag}
 
 # sudo nvidia-docker run --net host --ipc host -v /home/boren:/home/boren --memory 416G --name boren_dev speechpipelineregistry01.azurecr.io/cascades:official
 # sudo nvidia-docker exec -it cascades bash
--- run_docker.sh
+++ run_docker.sh
@@ -5,11 +5,17 @@
 # az acr login --resource-group devboxes -n sramdevregistry
 # run as uid:gid in container
 
-# docker stop boren_phi_dev && docker rm boren_phi_dev
-# docker run --rm -d --user boren --gpus all --ipc host -v /home/boren:/home/boren -v /mnt:/mnt -v /mnt2:/mnt2 --name boren_phi_dev sramdevregistry.azurecr.io/boren_dev:a100_dev tail -f /dev/null
-# docker exec -it boren_phi_dev bash
+# podman stop boren_phi_dev && podman rm boren_phi_dev
+# podman run --rm -d --user boren --gpus all --ipc host -v /home/boren:/home/boren -v /mnt:/mnt -v /mnt2:/mnt2 --name boren_phi_dev sramdevregistry.azurecr.io/boren_dev:a100_dev tail -f /dev/null
+# podman exec -it boren_phi_dev bash
 
-docker stop boren_trl && docker rm boren_trl
-docker run --rm -d --gpus all --ipc host -v /home/boren:/home/boren -v /mnt:/mnt -v /mnt2:/mnt2 --name boren_trl sramdevregistry.azurecr.io/boren_dev:trl tail -f /dev/null
-# docker run --rm -d --gpus all --ipc host -v /home/boren:/home/boren -v /mnt:/mnt -v /mnt2:/mnt2 --name boren_trl sramdevregistry.azurecr.io/boren_dev:a100_dev tail -f /dev/null
-docker exec -it boren_trl bash
+name=boren_trl
+# image=nvcr.io/nvidia/pytorch:25.01-py3
+# image=vastai/vllm:v0.8.5-cuda-12.4-pytorch-2.6.0-py312
+image=vllm/vllm-openai:latest
+podman run --rm -d --gpus all --ipc host -v /datablob1:/datablob1 -v /home/boren:/home/boren -v /mnt:/mnt -v /mnt2:/mnt2 --name boren_trl ${image} tail -f /dev/null
+podman exec -it ${name} bash
+
+# podman stop ${name} && podman rm ${name}
+# podman run --rm -d --gpus all --ipc host -v /home/boren:/home/boren -v /mnt:/mnt -v /mnt2:/mnt2 --name boren_trl sramdevregistry.azurecr.io/boren_dev:trl tail -f /dev/null
+# podman run --rm -d --gpus all --ipc host -v /home/boren:/home/boren -v /mnt:/mnt -v /mnt2:/mnt2 --name boren_trl sramdevregistry.azurecr.io/boren_dev:a100_dev tail -f /dev/null
