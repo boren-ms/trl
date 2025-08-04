a1c430e7b93933808980207a0249c6b8c2b42771
Bo Ren <boren@microsoft.com>
2025-08-04 06:19:40 +0000

    add fetch configs

--- fetch_result.sh
+++ fetch_result.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # This script is used to fetch results from a specific run of the GRPO trainer.
-python ./wandb_result.py --metric eval search grpo_ls_m1000_p9_n10_seed0_e2
+# python ./wandb_result.py --metric eval search grpo_ls_m1000_p9_n10_seed0_e2
+# python ./wandb_result.py --metric metric search grpo_ls_m1000_p9_n10_seed0_e2
+# python ./wandb_result.py --metric metric search grpo_ls_m1000_p9_n10_seed_e2_t12
+python ./wandb_result.py --metric metric_vllm search grpo_ls_m1000_p9_n10_zero_e2_t12
