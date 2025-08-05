6cb80c64ec40288895d0c4175d4bd24bac507c68
Bo Ren <boren@microsoft.com>
2025-08-05 01:06:54 +0000

    add result

--- fetch_result.sh
+++ fetch_result.sh
@@ -9,5 +9,6 @@
 # python ./wandb_result.py --metric metric_vllm search 5000_hf
 # python ./wandb_result.py --metric metric_vllm search grpo_ls_m1000_p9_n10_seed0_e2_t12_simple_err_lora_G2x8
 # python ./wandb_result.py --metric metric search grpo_ls_m1000_p9_n10_seed0_e2_t12_lower_err
-python ./wandb_result.py --metric metric_vllm search grpo_ls_m1000_p9_n10_zero_e2_t12_notag
+# python ./wandb_result.py --metric metric_vllm search grpo_ls_m1000_p9_n10_zero_e2_t12_notag
+python ./wandb_result.py --metric metric_vllm search Phi-4-multimodal-instruct
 
