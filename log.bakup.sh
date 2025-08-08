75a2a9ba7c442dce28a43c3b8b6710a91a50bdc6
Bo Ren <boren@microsoft.com>
2025-08-08 21:59:46 +0000

    update log

--- log.bakup.sh
+++ log.bakup.sh
@@ -13,8 +13,9 @@ ssh h-n1-uks7 -- 'pgrep -fa /launch_'
 ssh h-n2-hpe4 -- 'pgrep -fa /launch_'
 1046705 python3 ./launch_train.py orng_conf/biasing/grpo_hcv2_fy22_m1000_seed_e1_simple_err_ga8_t12_n10x2_sc3k_ctx5.yaml
 
+
 ssh h-n2-uks7 -- 'pgrep -fa /launch_'
-34608 python3 ./launch_train.py orng_conf/biasing/grpo_ls_m1000_zero_e1_simple_err3_ga8_t12_n10x2_sc3k_ctx8.yaml
+1351182 python3 ./launch_train.py orng_conf/biasing/grpo_hcv2_fy22_m1000_seed_egs300k_simple_err_ga8_t12_n10x2_sc3k_ctx5.yaml --seed_name grpo_hcv2_fy22_m1000_seed_egs300k_simple_err_ga8_t12_n10x2_sc3k_ctx5_G1x8
 
 ssh h-n2-wus2 -- 'pgrep -fa /launch_'
 796322 python3 ./launch_train.py orng_conf/biasing/grpo_ls_m1000_zero_e2_simple_err_ga8_t12_n10_sc3k_ctx9.yaml
