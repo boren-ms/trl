7a532d28c0ac6e65915880e6405ebb7fd780a79a
Bo Ren <boren@microsoft.com>
2025-08-10 03:38:31 +0000

    update log

--- log.bakup.sh
+++ log.bakup.sh
@@ -1,39 +1,39 @@
-dev-n1-hpe2 --
+dev-n1-hpe2
 ./launch_train.py orng_conf/biasing/done/grpo_ls_m1000_seed1k_e1_simple_err_ga8_t12_n10x2_sc3k_ctx8.yaml
 
-dev-n1-hpe5 --
-./launch_train.py orng_conf/biasing/done/grpo_ls_m1000_zero_e1_simple_err10_ga8_t12_n10x2_sc3k_ctx8.yaml
+dev-n1-hpe5
+launch_train.py orng_conf/biasing/done/grpo_ls_m1000_zero_e1_simple_err10_ga8_t12_n10x2_sc3k_ctx8.yaml
 
-dev-n1-uks7 --
+dev-n1-uks7
 ./launch_train.py orng_conf/biasing/done/grpo_hcv2_fy22_m1000_seed_egs300k_simple_err_ga8_t12_n10x2_sc3k_ctx8.yaml
 
-dev-n1-wus2 --
+dev-n1-wus2
 ./launch_train.py orng_conf/biasing/pending/grpo_ls_m1000_zero_e1_simple_err_ga8_t12_n20_sc3k_ctx8.yaml
 
-dev1-n1-hpe5 --
-./launch_eval.py grpo_ls_m1000_seed_e3_simple_err_ga8_t15_n10x2_sc3k_ctx5_G4x8
+dev1-n1-hpe5
+launch_eval.py grpo_ls_m1000_zero_e3_simple_err_ga8_t12_n10_sc3k_ctx9_G4x8
 
-h-n1-uks7 --
+h-n1-uks7
 ./launch_train.py orng_conf/biasing/grpo_ls_m1000_seed_e3_simple_err_ga8_t15_n10x2_sc3k_ctx9.yaml
 
-h-n2-hpe4 --
-./launch_train.py orng_conf/biasing/grpo_hcv2_fy22_m1000_seed_e1_simple_err_ga8_t12_n10x2_sc3k_ctx5.yaml
+h-n2-hpe4
+./launch_train.py orng_conf/biasing/done/grpo_hcv2_fy22_m1000_seed_e1_simple_err_ga8_t12_n10x2_sc3k_ctx5.yaml
 
-h-n2-uks7 --
+h-n2-uks7
 ./launch_train.py orng_conf/biasing/pending/grpo_ls_m1000_zero_e6_simple_err_ga8_t12_n10_sc3k_ctx8.yaml
 
-h-n2-wus2 --
-./launch_eval.py grpo_ls_m1000_zero_e1_simple_err_ga8_t12_n10_sc3k_ctx8_G1x8
+h-n2-wus2
+./launch_eval.py grpo_ls_m1000_seed_e1_simple_err_ga8_t15_n10x2_sc3k_ctx8_G2x8
 
-h-n4-hpe4 --
+h-n4-hpe4
 ./launch_train.py orng_conf/biasing/grpo_ls_m1000_seed_e6_simple_err_ga8_t15_n10x2_sc3k_ctx8.yaml
 
-h1-n2-hpe4 --
-./launch_train.py orng_conf/biasing/grpo_hcv2_fy22_m1000_zero_e1_simple_err_ga8_t12_n10x2_sc3k_ctx8.yaml
+h1-n2-hpe4
+./launch_train.py orng_conf/biasing/done/grpo_hcv2_fy22_m1000_zero_e1_simple_err_ga8_t12_n10x2_sc3k_ctx8.yaml
 
-h1-n2-wus2 --
+h1-n2-wus2
 ./launch_train.py orng_conf/biasing/grpo_ls_rare_zero_e3_simple_err_ga8_t12_n10x2_sc3k_ctx0.yaml
 
-l-n1-hpe2 --
-./launch_eval.py grpo_ls_m1000_zero_e3_simple_err_ga8_t12_n10_sc3k_ctx9_G2x8
+l-n1-hpe2
+./launch_eval.py grpo_ls_m1000_zero_e1_simple_err_ga8_t12_n10_sc3k_ctx5_G4x8
 
