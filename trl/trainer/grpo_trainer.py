fix logprob diff

trl/trainer/grpo_trainer.py
@@ -1451,9 +1451,9 @@ class GRPOTrainer(Trainer):
         self._textual_logs["advantages"].extend(advantages.tolist())
 
         if rollout_per_token_logps is not None and old_per_token_logps is not None:
-            logps_diff = torch.abs(old_per_token_logps - rollout_per_token_logps)
-            self._metrics[mode]["Token Probability Difference/max"].append(logps_diff.max().exp().item())
-            self._metrics[mode]["Token Probability Difference/mean"].append(logps_diff.mean().exp().item())
+            logps_diff = torch.abs(old_per_token_logps.exp() - rollout_per_token_logps.exp()) * completion_mask.int()
+            self._metrics[mode]["Token Probability Difference/max"].append(logps_diff.max().item())
+            self._metrics[mode]["Token Probability Difference/mean"].append(logps_diff.mean().item())
 
         return {
             "prompt_ids": prompt_ids,