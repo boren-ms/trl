0eed909275d1fcf2eedc9bd2baad7144d7fe0108
Bo Ren <boren@microsoft.com>
2025-08-21 17:29:47 +0000

    update configs

--- fetch_result.sh
+++ fetch_result.sh
@@ -21,4 +21,4 @@
 # python ./wandb_result.py --metric eval search 'grpo_ls_m1000_seed'
 # python ./wandb_result.py --dataset ls,entity --metric metric_vllm search 'Phi-4-multimodal-instruct|5000_hf|grpo_ls_m1000_p9_n10_seed_e2_t12_simple_err_lora|phi-libri_ft_m1000_p8_new-QpH|grpo_ls_m1000_p9_n10_zero_e2_t12_notag_simple_err_lora|grpo_ls_m1000_p9_n10_zero_e2_t12_notag'
 # python ./wandb_result.py  --metric eval search 'grpo_ls_m1000_seed_e1_simple_err'
-python ./wandb_result.py  --metric metric_vllm search 'grpo_ls_m1000_zero_e1_simple_err_'
+python ./wandb_result.py  --metric eval search 'grpo_ls_m1000_seed_e05_simple_err_t12_'
