89bc7dfdbf532141e66577c90d5a749e276ef323
Bo Ren <boren@microsoft.com>
2025-08-28 18:33:07 +0000

    update fetch

--- fetch_result.sh
+++ fetch_result.sh
@@ -6,7 +6,7 @@
 # python ./wandb_result.py --metric metric search grpo_ls_m1000_p9_n10_seed_e2_t12
 # python ./wandb_result.py --metric metric_vllm search grpo_ls_m1000_p9_n10_zero_e2_t12
 # python ./wandb_result.py --metric metric_vllm search grpo_ls_m1000_p9_n10_seed_e2_t12_simple_err_lora
-# python ./wandb_result.py --metric metric_vllm search 5000_hf
+python ./wandb_result.py --metric metric_vllm search 5000_hf
 # python ./wandb_result.py --metric metric_vllm search grpo_ls_m1000_p9_n10_seed0_e2_t12_simple_err_lora_G2x8
 # python ./wandb_result.py --metric metric search grpo_ls_m1000_p9_n10_seed0_e2_t12_lower_err
 # python ./wandb_result.py --metric metric_vllm search grpo_ls_m1000_p9_n10_zero_e2_t12_notag
@@ -21,4 +21,6 @@
 # python ./wandb_result.py --metric eval search 'grpo_ls_m1000_seed'
 # python ./wandb_result.py --dataset ls,entity --metric metric_vllm search 'Phi-4-multimodal-instruct|5000_hf|grpo_ls_m1000_p9_n10_seed_e2_t12_simple_err_lora|phi-libri_ft_m1000_p8_new-QpH|grpo_ls_m1000_p9_n10_zero_e2_t12_notag_simple_err_lora|grpo_ls_m1000_p9_n10_zero_e2_t12_notag'
 # python ./wandb_result.py  --metric eval search 'grpo_ls_m1000_seed_e1_simple_err'
-python ./wandb_result.py  --metric eval search 'grpo_ls_m1000_seed_e05_simple_err_t12_n16_bp8_imp5_G1x8|grpo_ls_m1000_seed_e4_simple_err_t12_n8_bp8_imp5_ref_sc1k_char_bias5_smp2a_G2x8|grpo_ls_m1000_seed_e2_simple_err_t12_n8_bp8_imp5_ref_sc1k_char_bias5_smp2a_G2x8|grpo_ls_m1000_seed_e05_simple_err_t12_n8_bp8_imp5_ref_sc1k_char_bias5_smp2a_G2x8|grpo_ls_m1000_seed_e05_simple_err_t12_n8_bp8_imp5_entropy50_G2x8'
+# python ./wandb_result.py  --metric eval search 'grpo_ls_m1000_seed_e05_simple_err_t12_n16_bp8_imp5_G1x8|grpo_ls_m1000_seed_e4_simple_err_t12_n8_bp8_imp5_ref_sc1k_char_bias5_smp2a_G2x8|grpo_ls_m1000_seed_e2_simple_err_t12_n8_bp8_imp5_ref_sc1k_char_bias5_smp2a_G2x8|grpo_ls_m1000_seed_e05_simple_err_t12_n8_bp8_imp5_ref_sc1k_char_bias5_smp2a_G2x8|grpo_ls_m1000_seed_e05_simple_err_t12_n8_bp8_imp5_entropy50_G2x8'
+
+
