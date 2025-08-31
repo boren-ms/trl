48960d1d0371b8f968583324bfdb6738c5b3702b
Bo Ren <boren@microsoft.com>
2025-08-31 01:48:32 +0000

    update fetch

--- fetch_result.sh
+++ fetch_result.sh
@@ -6,7 +6,7 @@
 # python ./wandb_result.py --metric metric search grpo_ls_m1000_p9_n10_seed_e2_t12
 # python ./wandb_result.py --metric metric_vllm search grpo_ls_m1000_p9_n10_zero_e2_t12
 # python ./wandb_result.py --metric metric_vllm search grpo_ls_m1000_p9_n10_seed_e2_t12_simple_err_lora
-python ./wandb_result.py --metric metric_vllm search 5000_hf
+# python ./wandb_result.py --metric metric_vllm search 5000_hf
 # python ./wandb_result.py --metric metric_vllm search grpo_ls_m1000_p9_n10_seed0_e2_t12_simple_err_lora_G2x8
 # python ./wandb_result.py --metric metric search grpo_ls_m1000_p9_n10_seed0_e2_t12_lower_err
 # python ./wandb_result.py --metric metric_vllm search grpo_ls_m1000_p9_n10_zero_e2_t12_notag
@@ -14,7 +14,7 @@ python ./wandb_result.py --metric metric_vllm search 5000_hf
 # python ./wandb_result.py --metric metric_vllm search grpo_ls_m1000_zero_e1_simple_err_ga8_t12_n10_sc3k_ctx8_G4x8
 # python ./wandb_result.py --metric metric_vllm search grpo_ls_m1000_p9_n10_zero_e2_t12_notag_simple_err3_lora_G2x8
 # python ./wandb_result.py --metric metric_vllm search Phi-4-multimodal-instruct
-# python ./wandb_result.py --metric metric_vllm search Phi4-7b-ASR-2506-v2
+python ./wandb_result.py --metric metric_vllm search grpo_ls_m1000_zero_e05_simple_err_t12_n8_bp8_imp5_G1x8
 # python ./wandb_result.py --metric eval search 'grpo_ls_m1000_seed_e3_simple_err_ga8_t15_n10x2_sc3k_ctx9_G1x8|grpo_ls_m1000_zero_e1_simple_err_ga8_t12_n10_sc3k_ctx8_G4x8'
 # python ./wandb_result.py --metric eval search 'grpo_ls_m1000_p9_n10_zero_e2_t12_notag|grpo_ls_m1000_zero_e1_simple_err_ga8_t12_n10_sc3k_ctx8_G4x8'
 # python ./wandb_result.py --metric eval search 'grpo_ls_m1000_p9_n10_seed_e2'
