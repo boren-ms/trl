fae2cce78664529b26494e8aed97b634567a2b17
Bo Ren <boren@microsoft.com>
2025-09-08 20:01:30 +0000

    update data proc

--- proc_ds.py
+++ proc_ds.py
@@ -1,40 +1,24 @@
 # %%
 from trl.scripts.audio_dataset import create_audio_dataset
-
+import yaml
+from pathlib import Path
 
 # %%
-common_file = "/home/boren/data/librispeech_biasing/words/all_words.count.txt"
-conf = {
-    "dataset_name": "chunk",
-    # "max_chunks": 2,
-    # "max_egs": 30,
-    "num_proc": 20,
-    "specs": [
-        "/home/boren/data/inhouse/data_spec/asr_chunk_inhouse_en.json",
-    ],
-    "add_rare_keywords": {
-        "common_file": common_file,
-        "common_num": 3000,
-    },
-    "filter_by_keywords": {
-        "min_ratio": 0.1,
-    },
-    "cache_name": "asr_chunk_inhouse_en_sc3k_0.1r",
-}
+conf_path = Path("/mnt2/newhome/boren/trl/orng_conf/biasing/data/hcv2_sc3k_fr01.yaml")
+
+conf = yaml.safe_load(conf_path.read_text())["train_data"]
+conf.update({"cache_name": conf_path.stem})
+print("Config: ", conf)
 
 ds = create_audio_dataset(**conf)
 print("Got dataset info")
 print(ds)
 print("dataset samples:")
-for i, egs in enumerate(ds):
-    n_words = len(egs["text"].split())
-    n_keywords = len(egs["keywords"])
-    print("index:", i)
-    print("text:", egs["text"])
-    print("keywords:", egs["keywords"])
-    print(f"num_words: {n_words}, num_keywords: {n_keywords}, ratio: {n_keywords/(n_words+1e-6):.3f}")
-    print()
-    if i >= 10:
-        break
+egs = ds[0]
+n_words = len(egs["text"].split())
+n_keywords = len(egs["keywords"])
+print("text:", egs["text"])
+print("keywords:", egs["keywords"])
+print(f"num_words: {n_words}, num_keywords: {n_keywords}, ratio: {n_keywords/(n_words+1e-6):.3f}")
 
 # %%
