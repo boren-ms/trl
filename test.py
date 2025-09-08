20524739e874597d5df4a0fe4e7e3ea0044c244d
Bo Ren <boren@microsoft.com>
2025-09-08 04:58:11 +0000

    update test.py

--- test.py
+++ test.py
@@ -20,14 +20,18 @@ print("mean:", sum(y) / len(y))
 print("max:", max(y))
 print("min:", min(y))
 # %%
-ds_path = "/home/boren/data/cache_datasets/asr_chunk_inhouse_en_fy22_sc3k_0.1r"
+ds_path = "/home/boren/data/cache_datasets/asr_chunk_inhouse_en_sc3k_0.1r"
 from datasets import load_from_disk
 
 ds = load_from_disk(ds_path)
+
+print(ds)
 # %%
 for i, egs in enumerate(ds):
+
     keywords = egs.get("keywords", None)
     words = set(egs.get("text", None).split())
+    print(" audio_chunk:", egs["audio_chunk"])
     print("  keywords:", keywords)
     print("  keywords rate:", len(keywords) / len(words) if words else 0)
     print("  words:", words)
