0c220a71d8734b61712e66e316ebd72ec14dbd8a
Bo Ren <boren@microsoft.com>
2025-09-12 23:35:41 +0000

    ner with ds

--- trl/scripts/ner_dataset.py
+++ trl/scripts/ner_dataset.py
@@ -1,9 +1,11 @@
 # pip install ray[default] transformers datasets torch
+from attrs import field
 import ray
 import torch
 from itertools import chain
 from more_itertools import divide
 from transformers import pipeline
+from transformers.pipelines.pt_utils import KeyDataset
 from datasets import load_dataset
 
 
@@ -32,8 +34,13 @@ class NERActor:
             model_kwargs={"torch_dtype": torch.bfloat16},
         )
 
-    def infer_batch(self, texts):
-        return [join_entities(outputs, text) for text, outputs in zip(texts, self.pipe(texts, batch_size=len(texts)))]
+    def infer_batch(self, ds, bs=8, key="text"):
+        outputs = []
+        ds = KeyDataset(ds, key)
+        for i, output in enumerate(self.pipe(ds, batch_size=bs)):
+            entities = [x["word"] for x in output]
+            outputs.append(entities)
+        return outputs
 
 
 _ACTORS = None
@@ -66,6 +73,18 @@ def ray_ner(texts, model_id, num_actors=0):
     return keywords
 
 
+def ner_ds(ds, model_id, src_field="text", tgt_field="keywords", bs=None, n_actors=0):
+    actors = _get_actors(model_id, n_actors)
+    futures = []
+    n = len(actors)
+    for i in range(n):
+        ds_i = ds.shard(num_shards=n, index=i, contiguous=True)
+        futures.append(actors[i].infer_batch.remote(ds_i, bs=bs, key=src_field))
+    results = ray.get(futures)
+    ds = ds.add_column(tgt_field, list(chain(*results)))
+    return ds
+
+
 def ner_map(batch, **kwargs):
     texts = batch["prompt"]
     if not texts:
@@ -80,16 +99,16 @@ def ner_map(batch, **kwargs):
 if __name__ == "__main__":
     # Example: load your HF dataset
     model_path = "/home/boren/data/ckp/hf_models/roberta-large-ner-english/"
-    num_actors = 2
+    n_actors = 2
     ds = load_dataset("fka/awesome-chatgpt-prompts", split="train")
-
-    ds2 = ds.map(
-        ner_map,
-        fn_kwargs={"model_path": model_path, "num_actors": num_actors},
-        batched=True,
-        num_proc=1,
-        desc="NER via Ray RPC",
-    )
+    ds2 = ner_ds(ds, model_path, n_actors, key="prompt")
+    # ds2 = ds.map(
+    #     ner_map,
+    #     fn_kwargs={"model_path": model_path, "num_actors": n_actors},
+    #     batched=True,
+    #     num_proc=1,
+    #     desc="NER via Ray RPC",
+    # )
     print(ds2)
     print(ds2[0])
 
