85f7beb3f1428a4aed06028fbab6ff1888673b70
Bo Ren <boren@microsoft.com>
2025-09-12 01:11:40 +0000

    update test

--- test.py
+++ test.py
@@ -1,41 +1,46 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 # %%
-
-import random
-
-
-def rand_int(min_val, max_val):
-    """Randomly sample an integer between min_val and max_val."""
-    mu = (min_val + max_val) / 2
-    sigma = (max_val - min_val) / 6
-    return int(random.gauss(mu, sigma))
+from transformers import pipeline
+import re
 
 
-print("Random number:", rand_int(0, 1000))
+def complete_prefix(text, prefix, case_sensitive=False):
+    flags = 0 if case_sensitive else re.IGNORECASE
+    pattern = r"\b" + re.escape(prefix) + r"\w*\b"
+    matches = re.findall(pattern, text, flags)
+    return matches[0] if matches else prefix
+
+
+model_id = "Jean-Baptiste/roberta-large-ner-english"
+ner = pipeline("ner", model=model_id, aggregation_strategy="simple")
+text = "It's a high level airbender move, with some spiritual stuff thrown in. That actually made me laugh a little because during the Korra Book 2 dvd commentaries. Mike and Bryan talked about Jinora's Raava rescue. They explained that they didn't know what she was doing. They just wanted to see her resuscitate the little piece of Raava that was inside UnaVaatu. But for all intents and purposes, think of the Air substyle as Spirit Bending. Not to"
+results = ner(text)
+
+
 # %%
-y = [rand_int(0, 1000) for i in range(10)]
-# %%
-print("List of random numbers:", y)
-# %%
-print("mean:", sum(y) / len(y))
-print("max:", max(y))
-print("min:", min(y))
-# %%
-ds_path = "/home/boren/data/cache_datasets/asr_chunk_inhouse_en_sc3k_0.1r"
-from datasets import load_from_disk
+def find_entity(text, ner):
+    words = [""]
+    last_e = 0
+    for res in ner(text):
+        s, e = res["start"], res["end"]
+        if not text[last_e:s].strip():
+            words[-1] += text[last_e:e]
+        else:
+            words.append(text[s:e])
+        last_e = e
+    words = set([w.strip() for w in words if w.strip()])
+    return words
 
-ds = load_from_disk(ds_path)
 
-print(ds)
+entities = find_entity(text, ner)
+print(entities)
+
 # %%
-for i, egs in enumerate(ds):
-
-    keywords = egs.get("keywords", None)
-    words = set(egs.get("text", None).split())
-    print(" audio_chunk:", egs["audio_chunk"])
-    print("  keywords:", keywords)
-    print("  keywords rate:", len(keywords) / len(words) if words else 0)
-    print("  words:", words)
-    if i > 10:
-        break
+# entities = set()
+# for segment in ner(text):
+#     word = segment["word"].strip()
+#     word = complete_prefix(text, word)
+#     entities.update(word.split())
 
 # %%
