fa7217fedf10d7748fa4302a985eacd2f198f78a
Bo Ren <boren@microsoft.com>
2025-09-14 01:02:41 +0000

    fix bug

--- proc_ds.py
+++ proc_ds.py
@@ -25,7 +25,7 @@ def proc_dataset(conf_path, push_remote=False):
     cache_path = f"~/data/cache_datasets/{conf_path.stem}"
     if push_remote:
         print(f"Syncing dataset [{cache_path}] to remote storage...")
-        sync_remote_dir(cache_path, True)
+        sync_remote_dir(cache_path, True).remote()
     print("dataset sample [0]:")
     egs = ds[0]
     n_words = len(egs["text"].split())
