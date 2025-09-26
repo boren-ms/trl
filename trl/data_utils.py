812c13672aac38e7903669e7faf2a8846e9386a4
Bo Ren <boren@microsoft.com>
2025-09-26 16:59:21 +0000

    update audio limit

--- trl/data_utils.py
+++ trl/data_utils.py
@@ -43,7 +43,15 @@ def sf_read(file_path):
     return audio, sr
 
 
-def load_audio(x):
+def limit_audio(x, fs, max_dur=30):
+    """Limit the length of the audio to max_dur seconds."""
+    if len(x) > fs * max_dur:
+        print(f"Truncating audio {len(x)/fs:.2f} ->  {max_dur} seconds.")
+        x = x[: fs * max_dur]
+    return x, fs
+
+
+def load_raw_audio(x):
     """Load audio data from the input dictionary."""
     if "audio" in x and "sr" in x:
         return x["audio"], x["sr"]
@@ -54,7 +62,12 @@ def load_audio(x):
     raise ValueError("No audio data found in the input dictionary.")
 
 
-def load_audios(x):  # x is batched
+def load_audio(x, max_dur=30):
+    data, fs = load_raw_audio(x)
+    return limit_audio(data, fs, max_dur=max_dur)
+
+
+def load_raw_audios(x):  # x is batched
     if "audio" in x and "sr" in x:
         return list(zip(x["audio"], x["sr"]))
     elif "audio_path" in x:
@@ -62,6 +75,10 @@ def load_audios(x):  # x is batched
     elif "audio_chunk" in x:
         return [load_chunk_example(p) for p in x["audio_chunk"]]
     raise ValueError("No audio data found in the input dictionary.")
+
+
+def load_audios(x, max_dur=30):  # x is batched
+    return [limit_audio(data, fs, max_dur=max_dur) for data, fs in load_raw_audios(x)]
 
 
 def is_conversational(example: dict[str, Any]) -> bool:
