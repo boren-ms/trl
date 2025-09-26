85060324c6a0d3d3d51bb0eb6b9c528befd89bb3
Bo Ren <boren@microsoft.com>
2025-09-26 01:12:52 +0000

    add test scritp

--- test.py
+++ test.py
@@ -22,3 +22,58 @@ results = ner(texts)
 #     entities.update(word.split())
 
 # %%
+import pandas as pd
+
+from pathlib import Path
+
+# %%
+data_dir = Path("~/data/librispeech_biasing/sunit_filter/20250919/").expanduser()
+raw_jsonl = data_dir / "metric_vllm_ls_other_2000__results.jsonl"
+sunit_jsonl = data_dir / "metric_vllm_ls_other_2000_sunit_th05__results.jsonl"
+
+df_raw = pd.read_json(raw_jsonl, lines=True)
+df_sunit = pd.read_json(sunit_jsonl, lines=True)
+# %%
+
+df = df_raw.merge(
+    df_sunit,
+    on=["id"],
+    suffixes=("_raw", "_sunit"),
+)
+
+
+# %%
+mdf = df[["id", "hyp_raw", "hyp_sunit", "prompt_raw", "prompt_sunit"]]
+from datasets import Dataset
+
+
+ds = Dataset.from_pandas(mdf)
+
+
+# %%
+def extract_words(text):
+    text = text.replace("<|user|><|audio_1|>Transcribe the audio clip into text. Pay extra attention to the following phrases/words. ", "")
+    text = text.replace("<|end|><|assistant|>", "")
+    return [w.strip().strip("*") for w in text.split(",")]
+
+
+def process(example):
+    input_words_raw = extract_words(example["prompt_raw"])
+    input_words_sunit = extract_words(example["prompt_sunit"])
+    diff = set(input_words_sunit) - set(input_words_raw)
+    return {
+        "diff": diff,
+        "input_words_raw": input_words_raw,
+        "input_words_sunit": input_words_sunit,
+    }
+
+
+ds = ds.map(process)
+# %%
+for example in ds:
+    diff = example["diff"]
+    if len(diff) == 0:
+        continue
+    print(f"Diff: {diff}")
+
+# %%
