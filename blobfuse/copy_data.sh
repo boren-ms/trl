0c02dd93a29ffdf04b4e275574593cd8bad14adf
Bo Ren <boren@microsoft.com>
2025-10-29 20:38:12 +0000

    update copy data

--- blobfuse/copy_data.sh
+++ blobfuse/copy_data.sh
@@ -33,12 +33,11 @@ dst_sas=$(get_sas ${dst_storage}_${dst_blob})
 
 # https://tsstd01uks.blob.core.windows.net/amulet/projects/phimm/amlt-results/7261045517.23695-d19a1453-dea3-4d4b-9e82-a60db2f8ce1d/540000/
 
-# Gaming_43k
-# K12_Higher_Education_36k
-# Retail_26k
-# Science_And_Tech_48k
-# Insurance_16k
-tag="Insurance_16k"
+tag=Gaming_43k
+# tag=K12_Higher_Education_36k
+# tag=Retail_26k
+# tag=Science_And_Tech_48k
+# tag=Insurance_16k
 src_rel_path="users/v-zhhon/en-US/chunk_dragon_display/speed_simulate_6chunk_display/${tag}/*"
 # src_rel_path=users/ruchaofan/wavllm_data/wavllm/converted_path_train_data_4chunk/asr_train_transcribe.tsv
 dst_rel_path=/home/boren/data/inhouse/entity_data/entity_chunk/${tag}/ChunkFiles
